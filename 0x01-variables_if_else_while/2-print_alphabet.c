4577e83b030b4a04709b984df01d1f6841fdf14b
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-21 10:37:29 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,14 @@
 /**
   * main - Prints the alphabet.
   *
-  * Return: Always 0 (Success)
+  * Return: Always 0 (Success).
   */
-
 int main(void)
 {
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i <26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alph[i]);
 	}
