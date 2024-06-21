3602cecbf6dbbbecd297e9c62596debfd4ed3738
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-21 09:57:30 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * main - Gets the last digit of a random generated number
+  * check if it is greater than 5, less than 6, or 0.
+  *
+  * Returns: Always 0 (seccess).
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, (n % 10);
+	}
+	return (0);
+}
