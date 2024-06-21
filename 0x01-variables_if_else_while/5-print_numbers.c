28abebc66a50f4137b73ac9e93bd857e02c0a1fd
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-21 11:03:17 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Print all single digit numbers from 0.
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
