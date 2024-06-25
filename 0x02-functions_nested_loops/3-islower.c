c9c46f5a0096379b82ea48459b43bcd96ba7659b
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-25 15:14:59 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * islower - checks for lowercase character.
+  *
+  * Return: 1 if lowercase, 0 otherwise.
+  */
+
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
