d7b59cd6ee65040036ec1335e1edb93bb53f55d6
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-25 20:15:23 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "natural.c"
+
+/**
+  * main - print all natural numbers multiplies by 3 or 5
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	print_sum_natural();
+	return (0);
+}
+
