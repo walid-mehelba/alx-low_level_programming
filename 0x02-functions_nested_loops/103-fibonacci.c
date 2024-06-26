dd28fb119736b598ccc803ff33a62216a4f44cd2
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-25 20:48:04 -0700

    fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,10 +2,9 @@
 
 /**
  * main - finds and prints the sum of the even-valued terms
- * 
- * Return: Always 0
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
@@ -25,6 +24,8 @@ int main(void)
 		j = k;
 		k = next;
 	}
+
 	printf("%lu\n", sum);
+
 	return (0);
 }
