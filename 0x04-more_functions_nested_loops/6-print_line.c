40d5c659314a9d857a6c57548d3d0d6b386b34ab
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-27 18:47:48 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
-  * print_line - draw straight line
-  * @n: nomber of character '-' to be printed
-  * Return: void
-  */
-
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
+		int i;
+
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('-');
+			_putchar('_');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
