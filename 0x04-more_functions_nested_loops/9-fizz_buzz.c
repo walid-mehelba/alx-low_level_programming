7d3259358443765d3a8ee246fdd72ca1b90cadfb
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-27 18:58:49 -0700

    fizbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - fizzbuzz
+  * Return: 0
+  */
+
+int main(void)
+{
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			prinf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
