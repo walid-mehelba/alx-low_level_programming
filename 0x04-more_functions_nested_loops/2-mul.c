b69360bf76bd7140474d4b2a80be66052b16d7f4
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-27 17:45:45 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - multiply two integers
+  * @a: first number
+  * @b: second number
+  * Return: the result
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
