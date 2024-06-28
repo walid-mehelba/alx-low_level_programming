e2a5bf020a868457a56fcaffce0c987a00ead740
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-06-27 17:57:57 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - print numbers from 0 to 9
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
