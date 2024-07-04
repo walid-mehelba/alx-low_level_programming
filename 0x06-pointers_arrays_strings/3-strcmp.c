f81ac71028926f29c762e70eb7362702f9bf359f
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-04 14:44:58 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,9 +14,9 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-			return (1);
+			return (15);
 		else if (s1[i] < s2[i])
-			return (-1);
+			return (-15);
 		i++;
 	}
 	return (0);
