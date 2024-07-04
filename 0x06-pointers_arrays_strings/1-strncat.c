b77bcf591f30dbaf1baa1a232b2fd4f95520bceb
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-04 10:04:10 -0700

    concat n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,20 +9,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *tmp = dest;
-	int i;
+	int i = 0, j = 0;
 
-	while (*dest != '\0')
+	while (dest[i] != '\0')
 	{
-		dest++;
+		i++;
 	}
 
-	for (i = 0; i <= n; i++)
+	while (j < n && src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	*dest = '\0';
-	return (tmp);
+	dest[i] = '\0';
+	return (dest);
 }
