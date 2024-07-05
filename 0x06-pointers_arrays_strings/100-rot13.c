bcf22e03d5cfafb603f7c8c21fd11b15b99afb9a
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-04 19:02:12 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * rot13 - encodes a string using rot13
+  * @str: string
+  * Return: pointer to the string
+  */
+
+char *rot13(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[i] != '\0')
+	{
+		while (s1[j] != '\0')
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
