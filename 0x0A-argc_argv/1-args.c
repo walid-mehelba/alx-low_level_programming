a2f46bf222ed40edb7f9b22ee578b3905544225f
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-12 17:45:21 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - print program name
+  * @argc: args counts
+  * @argv: args vector
+  * Return: always 0 success
+  */
+
+int main(int argc, char *argv[] __attribute((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
