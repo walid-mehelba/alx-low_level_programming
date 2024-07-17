1fe9cf456ce3a223a3a5d0aa8c008f66449b8119
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-16 18:32:00 -0700

    2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		mee[i] = malloc(sizeof(int) * width);
+		p[i] = malloc(sizeof(int) * width);
 
 		if (p[i] == NULL)
 		{
