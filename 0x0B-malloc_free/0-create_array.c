73274d0a69e9f2a4313ac834e7e044e13f601044
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-16 17:18:31 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,10 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *p;
-
+	
+	p = malloc(sizeof(char) * size);
 	if (size == 0 || p == NULL)
 		return (NULL);
-
-	p = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 	{
