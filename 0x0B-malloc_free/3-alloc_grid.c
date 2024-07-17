cccb0c15fde7f3813a1155041bcaf19f13716585
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-16 18:31:02 -0700

    2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - allocate 2 dim arrays of integers
+  * @width: width of array
+  * @height: height of array
+  * Return: pointer to the array
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = malloc(sizeof(int *) * height);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		mee[i] = malloc(sizeof(int) * width);
+
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+
+			free(p);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+
+	return (p);
+}
+}
+
