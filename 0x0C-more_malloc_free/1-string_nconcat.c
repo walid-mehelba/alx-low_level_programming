d47247fcf169040ee84728d53af25d91e84d6acd
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-18 18:43:19 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i = 0, j;
+	unsigned int i = 0, j = 0;
 	unsigned int counter1 = 0, counter2 = 0;
 
 	if (s1 == NULL)
