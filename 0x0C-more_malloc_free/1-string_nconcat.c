5c595d3667b2d4623d070b7910cd294f0245d731
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-18 18:15:27 -0700

    nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * string_nconcat - concate two strings with n bytes
+  * @s1: first string
+  * @s2: second string
+  * @n: number of bytes
+  * Return: pointer to the new string
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i = 0, j;
+	unsigned int counter1 = 0, counter2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
+	while (s1[counter1])
+		counter1++;
+	while (s2[counter2])
+		counter2++;
+
+	if (n < counter2)
+		p = malloc(sizeof(char) * (counter1 + n + 1));
+	else
+		p = malloc(sizeof(char) * (coutner1 + counter2 + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	while (s1[i])
+	{
+		p[i] = s[i];
+		i++;
+	}
+
+	for (j = 0; j < n; j++)
+		p[i] = s2[j];
+
+	p[i] = '\0';
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "1-string_nconcat.c"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
