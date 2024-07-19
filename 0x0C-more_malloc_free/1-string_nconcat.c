388427582d061d1774bd90fc47df790d0b8cadc3
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-18 18:44:31 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,45 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
-  * string_nconcat - concate two strings with n bytes
-  * @s1: first string
-  * @s2: second string
-  * @n: number of bytes
-  * Return: pointer to the new string
-  */
-
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int i = 0, j = 0;
-	unsigned int counter1 = 0, counter2 = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	
-	while (s1[counter1])
-		counter1++;
-	while (s2[counter2])
-		counter2++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (n < counter2)
-		p = malloc(sizeof(char) * (counter1 + n + 1));
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		p = malloc(sizeof(char) * (counter1 + counter2 + 1));
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (p == NULL)
+	if (!s)
 		return (NULL);
 
-	while (s1[i])
+	while (i < len1)
 	{
-		p[i] = s1[i];
+		s[i] = s1[i];
 		i++;
 	}
 
-	while (n < counter2 && i < (counter1 + n))
-		p[i++] = s2[j++];
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	while (n >= counter2 && i < (counter1 + counter2))
-		p[i++] = s2[j++];
-	
-	p[i] = '\0';
-	return (p);
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
