934daa02e50866a50c3e1a903c1dded53b3cd0fb
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-22 18:33:54 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+  * struct dog - dog basic info
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  * Description: longer description
+  */
+
 struct dog {
 	char *name;
 	char *owner;
