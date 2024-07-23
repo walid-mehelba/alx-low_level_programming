df6e3fcbff61fff240db372334ddf241ff279b75
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-22 18:46:39 -0700

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
   * init_dog - initialize variable of type struct dog
@@ -10,7 +11,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name - name;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
