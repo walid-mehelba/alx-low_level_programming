cba7ddd0d185048b24a1b6fb24e2fa4353aff680
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-07-24 19:04:50 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - search for int inside array
+  * @array: array
+  * @size: size of array
+  * @cmp: pointer to function
+  * Return: index of the int
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int result;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		result = cmp(array[i]);
+
+		if (result != 0)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
