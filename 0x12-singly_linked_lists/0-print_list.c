fdc590a53fad590dc166f9b578eab3eb595b8003
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-08-01 17:38:47 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  * print_list - print all element in list
+  * @h: pointer to the list
+  * Return: number of nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	do
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	while (h != NULL);
+
+	return (count);
+}
+
