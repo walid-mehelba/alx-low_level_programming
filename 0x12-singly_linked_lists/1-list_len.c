63d99ccd96da5df9910ab4194c41b79b7b4b1548
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-08-01 18:37:44 -0700

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * list_len - find number of elements in linked list
-  * @h: pointer to start of the list
-  * Return: number of elements
-  */
-
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t n = 0;
 
-	while(h)
+	while (h)
 	{
-		count++;
-		h = h-> next;
+		n++;
+		h = h->next;
 	}
-	return (count);
+	return (n);
 }
