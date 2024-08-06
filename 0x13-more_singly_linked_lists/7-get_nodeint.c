211dff02a012ebad88b0ec5ab0051e82effbba40
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-08-06 16:51:57 -0700

    node nth

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,5 +18,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		n++;
 	}
 
-	return (temp ? temp : NULL):
+	return (temp ? temp : NULL);
 }
--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -5,6 +5,7 @@
 #include "5-free_listint2.c"
 #include "3-add_nodeint_end.c"
 #include "0-print_listint.c"
+#include "7-get_nodeint.c"
 /**
  * main - check the code
  *
@@ -13,6 +14,7 @@
 int main(void)
 {
     listint_t *head;
+    listint_t *node;
 
     head = NULL;
     add_nodeint_end(&head, 0);
@@ -24,7 +26,10 @@ int main(void)
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
+    node = get_nodeint_at_index(head, 5);
+    printf("%d\n", node->n);
+    print_listint(head);
     free_listint2(&head);
-    printf("%p\n", (void *)head);
+    return (0);
     return (0);
 }
