e39445436fe66a75e93a54bfa3726c6a41d290f4
walid-mehelba <walid_mehelba@hotmail.comgit commit -m 'My first commit'git config --global user.email you@example.com  git config --global user.name Your>
2024-08-06 16:03:13 -0700

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,6 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
 
@@ -19,10 +20,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	while (head)
-		head = head->next;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
-	head->next = new;
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
 
 	return (new);
 }
--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-#include "2-add_nodeint.c"
+#include "3-add_nodeint_end.c"
 #include "0-print_listint.c"
 /**
  * main - check the code
@@ -14,14 +14,14 @@ int main(void)
     listint_t *head;
 
     head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
     print_listint(head);
     return (0);
 }
